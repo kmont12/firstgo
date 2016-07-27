c7978004e2c0c2494ca88deadc53561045f5822e
keith <drinking.better.beer@gmail.com>
2016-07-26 22:35:12 -0400

    can now open files stored in docs

--- basicWebServer.go
+++ basicWebServer.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import ("net/http"
+	"log"
+	"io/ioutil"
+)
 
 func main(){
 	http.HandleFunc("/",handler)
@@ -8,5 +11,20 @@ func main(){
 }
 
 func handler(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("Hello\nWorld"))
+	path := r.URL.Path[1:]
+	log.Println(path)
+
+	if path==""{
+		path="index.html"
+	}
+
+	 data, err := ioutil.ReadFile("docs/"+string(path))
+
+
+	if err==nil{
+		w.Write(data)
+	} else{
+		w.WriteHeader(404)
+		w.Write([]byte("404 this didnt work" + http.StatusText(404)))
+	}
 }
