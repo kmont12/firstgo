cffc6eacefa33da7e609822c54763c2967e62424
keith <drinking.better.beer@gmail.com>
2016-07-27 20:28:33 -0400

    updated html and css; added content-type to go

--- basicWebServer.go
+++ basicWebServer.go
@@ -3,6 +3,7 @@ package main
 import ("net/http"
 	"log"
 	"io/ioutil"
+	"strings"
 )
 
 func main(){
@@ -12,16 +13,32 @@ func main(){
 
 func handler(w http.ResponseWriter, r *http.Request){
 	path := r.URL.Path[1:]
-	log.Println(path)
-
 	if path==""{
 		path="index.html"
 	}
 
 	 data, err := ioutil.ReadFile("docs/"+string(path))
 
-
+	log.Println(path)
 	if err==nil{
+		var contentType string
+		if strings.HasSuffix(path, ".css"){
+			contentType="text/css"
+		} else if strings.HasSuffix(path, ".html"){
+			contentType="text/html"
+                } else if strings.HasSuffix(path, ".js"){
+                        contentType="application/javascript"
+                } else if strings.HasSuffix(path, ".png"){
+                        contentType="image/png"
+                } else if strings.HasSuffix(path, ".svg"){
+                        contentType="image/svg+xml"
+		} else if strings.HasSuffix(path, ".jpg"){
+			contentType="image/jpeg"
+		} else {
+			contentType="text/plain"
+		}
+
+		w.Header().Add("Content-Type", contentType)
 		w.Write(data)
 	} else{
 		w.WriteHeader(404)
